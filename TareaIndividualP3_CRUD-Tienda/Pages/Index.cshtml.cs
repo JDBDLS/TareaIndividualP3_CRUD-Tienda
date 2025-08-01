b4325ecadb8ad1aca7c9cf5c26fc362a03ca8db8
Josué Bautista <josuedanielbautista@gmail.com>
2025-08-01 16:41:56 -0400

    Añadiendo lista de productos a la pagina index

--- TareaIndividualP3_CRUD-Tienda/Pages/Index.cshtml.cs
+++ TareaIndividualP3_CRUD-Tienda/Pages/Index.cshtml.cs
@@ -1,20 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TareaIndividualP3_CRUD_Tienda.Data;
+using TareaIndividualP3_CRUD_Tienda.Models;
 
 namespace TareaIndividualP3_CRUD_Tienda.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        public List<Product> Products { get; set; } = new();
 
         public void OnGet()
         {
-
+            Products = ProductRepository.Products;
         }
     }
 }
